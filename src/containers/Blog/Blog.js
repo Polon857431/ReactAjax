9d0decd1cb403fc42235c2379756a3b1a077ce61
Karol <karol.karwoski@wp.pl>
2019-06-06 09:12:43 +0200

    Add full post rendering

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -32,10 +33,19 @@ class Blog extends Component {
             );
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId:id});
+    }
+
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} author={post.author}/>
-        })
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}
+            />
+        });
 
         return (
             <div>
@@ -43,7 +53,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost/>
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost/>
